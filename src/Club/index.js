59a9900a2cd6e91fa1046c7efeda513e7ad946f7
krishnapavani123 <krishnapavani909@gmail.com>
2024-11-06 23:23:59 +0530

    Update index.js

--- src/Club/index.js
+++ src/Club/index.js
@@ -29,7 +29,7 @@ const Club=()=>{
                 <div className='sub-m'>
                 <h1 className='main-h'>“Why do we love our club? We love how creative it is: The customer has full control of the wines they want, when they want them, at the price they choose. How easy is that?”
                 </h1>
-                <img src="https://images.squarespace-cdn.com/content/v1/5a8207ded0e628cdcc5112d7/1522865252438-EBHCU7P2RTSZDGMUHRUU/ayres-vineyard-kathleen-co-owner-script.png?format=500w" className='img-m'/>
+                <img src="https://images.squarespace-cdn.com/content/v1/5a8207ded0e628cdcc5112d7/1522865252438-EBHCU7P2RTSZDGMUHRUU/ayres-vineyard-kathleen-co-owner-script.png?format=500w" className='img-m'  alt="imagec"/>
             </div>
             <div className='foot'>
             <p className='foot-p'>As a member of our case club, you will receive:</p>
